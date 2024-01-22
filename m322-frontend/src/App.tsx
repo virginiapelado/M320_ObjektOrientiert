dbe2b33f4294775ed3a9db28b3a952d27bc6693d
Virginia Pelado <virginia.pelado@noseryoung.com>
2024-01-22 15:49:53 +0100

    start details page

--- m322-frontend/src/App.tsx
+++ m322-frontend/src/App.tsx
@@ -7,6 +7,7 @@ import HomePage from "./Components/Pages/HomePage";
 import LoginPage from "./Components/Pages/LoginPage";
 import QuizPage from "./Components/Pages/QuizPage";
 import SignupPage from "./Components/Pages/SignupPage";
+import MovieDetailsPage from "./Components/Pages/MovieDetailsPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path="*" element={<PageNotFound />} />
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/quiz" element={<QuizPage />} />
+      <Route path="/details" element={<MovieDetailsPage />} />  {/* add /:id to details  */}
     </Routes>
   );
 }
