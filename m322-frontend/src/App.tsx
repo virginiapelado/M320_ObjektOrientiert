aca0e4c8b94b5cc9d760f3905ee05b398b17e0d7
Virginia Pelado <virginia.pelado@noseryoung.com>
2024-01-22 08:28:32 +0100

    Merge changes

--- m322-frontend/src/App.tsx
+++ m322-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./Components/Pages/PageNotFound";
 import ExplorePage from "./Components/Pages/ExplorePage";
 import HomePage from "./Components/Pages/HomePage";
+import QuizPage from "./Components/Pages/QuizPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/movie" element={<ExplorePage />} />
       <Route path="/movie/:id" element={<PageNotFound />} />
       <Route path="/login" element={<PageNotFound />} />
+      <Route path="/quiz" element={<QuizPage />} />
       <Route path="*" element={<PageNotFound />} />
       <Route path="/" element={<Navigate to="/home" />} />
     </Routes>
---
+++ m322-frontend/src/Components/Atoms/QuizQuestion.tsx
@@ -0,0 +1,10 @@
+export default function QuizQuestion() {
+    return(
+    <div className="question">
+        <p>Don't know what to watch?</p>
+        <p>Take this short quiz and get a recommendation from us!</p>
+    </div>    
+    )
+        
+
+}
