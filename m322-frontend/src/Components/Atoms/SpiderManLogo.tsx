262ea65e3ae0f3e85ba491aa3b9d239871f04faa
em <113352515+emmabrght@users.noreply.github.com>
2024-01-22 09:35:16 +0100

    todo

--- m322-frontend/src/Components/Atoms/SpiderManLogo.tsx
+++ m322-frontend/src/Components/Atoms/SpiderManLogo.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../../img/spider-man-logo.png"
 import { Link } from "react-router-dom";
 
+//TODO: route the button to the films page
+
 export default function SpiderManLogo() {
   return (
     <Link to="/*" style={{ textDecoration: "none" }}>
