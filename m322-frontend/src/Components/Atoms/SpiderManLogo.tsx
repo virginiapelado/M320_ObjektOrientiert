04f59419d1cdfee8386864e08e510ce55ab6cfe3
em <113352515+emmabrght@users.noreply.github.com>
2024-01-28 19:39:40 +0100

    scrollbar for aabish

--- m322-frontend/src/Components/Atoms/SpiderManLogo.tsx
+++ m322-frontend/src/Components/Atoms/SpiderManLogo.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import logo from "../../img/spider-man-logo.png"
-import { Link } from "react-router-dom";
-
-//TODO: route the button to the films page
+import logo from "../../img/spider-man-logo.png";
 
 export default function SpiderManLogo() {
-  return (
-    <Link to="/*" style={{ textDecoration: "none" }}>
-      <img src={logo} alt="Logo" className="spiderman-logo" />
-    </Link>
-  );
+  return <img src={logo} alt="Logo" className="spiderman-logo" />;
 }
--- m322-frontend/src/Components/Pages/MovieDetailsPage.tsx
+++ m322-frontend/src/Components/Pages/MovieDetailsPage.tsx
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import Header from "../Organisms/Header";
 import MovieDetails from "../Organisms/MovieDetails";
 import "./style.css";
+import { Movie } from "../../Service/Movie";
 
 const MovieDetailsPage = () => {
   return (
-    <div className="movie-details-page">
+    <div
+      className="movie-details-page"
+      style={{ height: "100vh", overflow: "scroll", overflowX: "hidden" }}>
       <div className="details-page-body">
         <Header />
         <MovieDetails />
