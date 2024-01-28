ce2a0bf544e4580c950c35a2a47bdffc9f4a8937
em <113352515+emmabrght@users.noreply.github.com>
2024-01-28 19:17:15 +0100

    Explore buttons

--- m322-frontend/src/Components/Atoms/ThumbnailCard.tsx
+++ m322-frontend/src/Components/Atoms/ThumbnailCard.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./style.css";
 import { Movie } from "../../Service/Movie";
+import { Link } from "react-router-dom";
 
 export default function ThumbnailCard(movie: Movie) {
   return (
     <div className="image">
-      <img
-        className="thumbnail-card"
-        alt="Thumbnail card"
-        src={movie.thumbnail}
-      />
+      <Link to={`/explore/${movie.id}`}>
+        <img
+          className="thumbnail-card"
+          alt="Thumbnail card"
+          src={movie.thumbnail}
+        />
+      </Link>
     </div>
   );
 }
