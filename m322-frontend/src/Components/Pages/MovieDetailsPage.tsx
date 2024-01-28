3e5272fdb65f663b5b89e39bd5878b896522fb51
em <113352515+emmabrght@users.noreply.github.com>
2024-01-28 20:30:19 +0100

    Added background for movie details

--- m322-frontend/src/Components/Pages/MovieDetailsPage.tsx
+++ m322-frontend/src/Components/Pages/MovieDetailsPage.tsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "../Organisms/Header";
 import MovieDetails from "../Organisms/MovieDetails";
 import "./style.css";
 import { Movie } from "../../Service/Movie";
+import { useNavigate, useParams } from "react-router-dom";
+import MovieService from "../../Service/MovieService";
 
 const MovieDetailsPage = () => {
+  const navigate = useNavigate();
+  let { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState<Movie>();
+
+  useEffect(() => {
+    if (id) {
+      MovieService()
+        .getMovieById(id)
+        .then((movieDetails) => {
+          setMovieDetails(movieDetails);
+        })
+        .catch(() => navigate("/explore", { replace: true }));
+    }
+  }, [id, navigate]);
+
   return (
     <div
       className="movie-details-page"
-      style={{ height: "100vh", overflow: "scroll", overflowX: "hidden" }}>
+      style={{
+        height: "100vh",
+        overflow: "scroll",
+        overflowX: "hidden",
+        background: `url('${movieDetails?.image!}`,
+      }}>
       <div className="details-page-body">
         <Header />
         <MovieDetails />
