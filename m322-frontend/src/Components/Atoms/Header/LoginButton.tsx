688edfafcc06afffff7635102c7a607d6e4bc763
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2024-01-29 14:27:26 +0100

    loginbutton update

--- m322-frontend/src/Components/Atoms/Header/LoginButton.tsx
+++ m322-frontend/src/Components/Atoms/Header/LoginButton.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
-import PersonIcon from '@mui/icons-material/Person';
+import { Link, useLocation } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
 
 export default function LoginButton() {
   const defaultColor = "#463F3A";
   const hoverColor = "#3d342e";
+  const activeColor = "#D17B34";
+
+  const location = useLocation();
+  const isActive = location.pathname === "/login";
 
   return (
     <Link to="/login" style={{ textDecoration: "none" }}>
-    <Button
-      sx={{
-        background: defaultColor,
-        ":hover": {
-          background: hoverColor,
-        },
-        width: "40px",
-        height: "40px",
-        flexShrink: 0,
-        borderRadius: "15px",
-        color: "white",
-        fontSize: "18px",
-      }}
-    >
-      <PersonIcon />
-    </Button>
+      <Button
+        sx={{
+          background: isActive ? activeColor : defaultColor,
+          ":hover": {
+            background: isActive ? activeColor : hoverColor,
+          },
+          width: "40px",
+          height: "40px",
+          flexShrink: 0,
+          borderRadius: "15px",
+          color: "white",
+          fontSize: "18px",
+        }}
+      >
+        <PersonIcon />
+      </Button>
     </Link>
   );
 }
