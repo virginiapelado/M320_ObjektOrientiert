f88f926227b19612e81d6511db2b0d334607019b
aabishtkhh <aabish.khan@edu.tbz.ch>
2024-01-29 08:40:28 +0100

    adding search function for virginia

--- m322-frontend/src/Components/Atoms/Header/SearchBar.tsx
+++ m322-frontend/src/Components/Atoms/Header/SearchBar.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+// SearchBar.tsx
+import React, { useState, ChangeEvent } from "react";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function SearchBar() {
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
   return (
     <TextField
       id="outlined-basic"
@@ -17,13 +32,19 @@ export default function SearchBar() {
       InputProps={{
         endAdornment: (
           <SearchIcon
+            onClick={handleSearch}
             style={{
               color: "#463F3A",
               padding: "5px",
+              cursor: "pointer",
             }}
           />
         ),
       }}
+      value={searchQuery}
+      onChange={handleInputChange}
     />
   );
-}
+};
+
+export default SearchBar;
