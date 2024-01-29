b0f537d5592fd7784c56fc35e6b43996c14f45df
aabishtkhh <aabish.khan@edu.tbz.ch>
2024-01-29 10:37:59 +0100

    review type added id

--- m322-frontend/src/Service/Reviews.ts
+++ m322-frontend/src/Service/Reviews.ts
@@ -2,9 +2,10 @@ import { Movie } from "./Movie";
 import { User } from "./User";
 
 export interface Reviews {
-    rating : number,
-    reviewTitle : string,
-    description : string,
-    movie : Movie
-    userReviews : User
+  id: string;
+  rating: number;
+  reviewTitle: string;
+  description: string;
+  movie: Movie;
+  userReviews: User;
 }
