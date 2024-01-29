966dc1c26f5175fda0801e354685f02b20c28d26
Emma Bright <113352515+emmabrght@users.noreply.github.com>
2024-01-29 09:24:56 +0100

    Set up area for reviews

--- m322-frontend/src/Components/Molecules/MovieInfoBox.tsx
+++ m322-frontend/src/Components/Molecules/MovieInfoBox.tsx
@@ -1,32 +1,38 @@
 import { Movie } from "../../Service/Movie";
-import GenreDisplay from "../Atoms/GenreDisplay";
 import "./MovieInfoBox.css";
 
 export default function MovieInfoBox(movie: Movie) {
   return (
-    <div className="details-box">
-      <div className="first-half">
-        <h1 className="movie-data">{movie.movieTitle}</h1>
-        <div className="info-data">{movie.movieDesc}</div>
+    <div className="MovieInfoBox">
+      <div className="details-box">
+        <div className="first-half">
+          <h1 className="movie-data">{movie.movieTitle}</h1>
+          <div className="info-data">{movie.movieDesc}</div>
+        </div>
+        <div className="second-half">
+          <div className="movie-info">
+            <div className="info-label">
+              <h2>Released</h2>
+            </div>
+            <div className="info-data">{movie.release_date}</div>
+          </div>
+          <div className="movie-info">
+            <div className="info-label">
+              <h2>Directors</h2>
+            </div>
+            <div className="info-data">{movie.director}</div>
+          </div>
+          <div className="movie-info">
+            <div className="info-label">
+              <h2>Length</h2>
+            </div>
+            <div className="info-data">{movie.movieLength} minutes</div>
+          </div>
+        </div>
       </div>
-      <div className="second-half">
-        <div className="movie-info">
-          <div className="info-label">
-            <h2>Released</h2>
-          </div>
-          <div className="info-data">{movie.release_date}</div>
-        </div>
-        <div className="movie-info">
-          <div className="info-label">
-            <h2>Directors</h2>
-          </div>
-          <div className="info-data">{movie.director}</div>
-        </div>
-        <div className="movie-info">
-          <div className="info-label">
-            <h2>Length</h2>
-          </div>
-          <div className="info-data">{movie.movieLength} minutes</div>
+      <div className="additional-info">
+        <div className="reviews">
+          <h1>Reviews</h1>
         </div>
       </div>
     </div>
